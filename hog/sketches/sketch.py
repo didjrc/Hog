0920732e7be66238ce5176fcb2442f31c9794979
didjrc <didjrc@nus.edu.sg>
2015-10-15 12:08:45 +0800

    Changed Directory Name

---
+++ hog/sketches/sketch.py
@@ -0,0 +1,15 @@
+""" Checks for prime numbers 
+
+To find all the prime numbers less than or equal to a given integer n by Eratosthenes' method:
+
+Create a list of consecutive integers from 2 through n: (2, 3, 4, ..., n).
+Initially, let p equal 2, the first prime number.
+Starting from p, enumerate its multiples by counting to n in increments of p, 
+and mark them in the list (these will be 2p, 3p, 4p, ... ; the p itself should not be marked).
+Find the first number greater than p in the list that is not marked. If there was no such number, stop. 
+
+Otherwise, let p now equal this new number (which is the next prime), and repeat from step 3.
+
+"""
+
+p = list(range(2,71,1))
